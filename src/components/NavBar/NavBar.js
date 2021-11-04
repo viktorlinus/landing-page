46a3ad92bfa3380f4d1238eec38900b24823d919
viktorlinus <viktorlinus@hotmail.com>
2021-11-04 16:14:55 +0100

    initial prep

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import './NavBar.css';
+
+const NavBar = () => {
+	return (
+		<div className='sticky'>
+	        <nav class="">
+	        	<ul class="main-nav">
+	         	 <li><strong><a href="#!">EKEN</a></strong></li>
+	         	 <li><a href="#!">Home</a></li>
+	      		</ul>
+	      	</nav>
+    	</div>
+	);
+}
+
+
+export default NavBar;
